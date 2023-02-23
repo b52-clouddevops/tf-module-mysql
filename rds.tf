d044be7199313057fd6670e115044abbef8d8a7b
CodingManoj <mvp2612@hotmail.com>
2023-02-22 19:54:42 -0500

    Auto Push Commit

--- rds.tf
+++ rds.tf
@@ -6,8 +6,8 @@ resource "aws_db_instance" "mysql5" {
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
-  username             = "foo"
-  password             = "foobarbaz"
+  username             = "admin1"
+  password             = "RoboShop@1"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
 }
