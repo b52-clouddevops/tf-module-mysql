067a81e6fa90429c16ddb9eed9c6a40d137486b4
CodingManoj <mvp2612@hotmail.com>
2023-02-22 20:18:22 -0500

    Auto Push Commit

--- schema.tf
+++ schema.tf
@@ -1,8 +1,8 @@
-resource "null_resource" {
+resource "null_resource" "mysql-schema" {
   
   # This is how we can create depenency and ensure this will only run after the creation if the RDS Instance.
   depends = [aws_db_instance.mysql5]
-  
+
   provisioner "local-exec" {
         command = <<EOF
         cd /tmp 
