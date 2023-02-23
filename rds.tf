cb35e2bbb8641497f6c5aaa09e12adab2c11b7f5
CodingManoj <mvp2612@hotmail.com>
2023-02-22 19:59:55 -0500

    Auto Push Commit

--- rds.tf
+++ rds.tf
@@ -11,6 +11,7 @@ resource "aws_db_instance" "mysql5" {
   parameter_group_name = aws_db_parameter_group.mysql.name
   db_subnet_group_name = aws_db_subnet_group.mysql.name
   skip_final_snapshot  = true
+  vpc_security_group_ids =
 }
 
 
