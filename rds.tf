18be67e0cfce672a01afc80ccb51aad93bb03091
CodingManoj <mvp2612@hotmail.com>
2023-02-22 19:53:25 -0500

    Auto Push Commit

--- rds.tf
+++ rds.tf
@@ -2,7 +2,7 @@
 
 resource "aws_db_instance" "mysql5" {
   allocated_storage    = 10
-  db_name              = "mydb"
+  identifier              = "mydb"
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
