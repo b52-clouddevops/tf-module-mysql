2bde5b95db0189829f88e7a3d6675afe6acae059
CodingManoj <mvp2612@hotmail.com>
2023-02-22 20:10:33 -0500

    Auto Push Commit

--- schema.tf
+++ schema.tf
@@ -2,6 +2,9 @@ resource "null_resource" {
   provisioner "local-exec" {
         command = <<EOF
 cd /tmp 
+curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+unzip mysql.zip 
+cd mysql-main 
 
 
 EOF
