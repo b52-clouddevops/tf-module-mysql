71a7a9660b71c3b7bbfb5cc362e9915cb4451925
CodingManoj <mvp2612@hotmail.com>
2023-02-24 20:02:55 -0500

    Auto Push Commit

--- schema.tf
+++ schema.tf
@@ -9,7 +9,7 @@ resource "null_resource" "mysql-schema" {
         curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
         unzip -o mysql.zip 
         cd mysql-main 
-        mysql -h ${aws_db_instance.mysql5.address} -u ${local.RDS_USER} -pRoboShop1 <shipping.sql
+        mysql -h ${aws_db_instance.mysql5.address} -u ${local.RDS_USER} -p${local.RDS_PASS} <shipping.sql
         EOF
   }  
 }
