35020ddb42f455938443e80d8374cf691323a4b4
CodingManoj <mvp2612@hotmail.com>
2023-02-24 19:59:32 -0500

    Auto Push Commit

--- data.tf
+++ data.tf
@@ -7,3 +7,23 @@ data "terraform_remote_state" "vpc" {
         region = "us-east-1"
   }
 }
+
+# Whenever you have some common fields or anything which is highly used, you can consider that in locals and can call it on your need
+
+locals {
+    DOCDB_USER = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_USERNAME"]
+    DOCDB_PASS = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_PASSWORD"]
+}
+
+# fetching the metadata of the secret
+data "aws_secretsmanager_secret" "secrets" {
+  name = "robotshop/secrets"
+}
+
+data "aws_secretsmanager_secret_version" "secrets" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
+
+output "data" {
+  value  =  jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_USERNAME"]
+}
